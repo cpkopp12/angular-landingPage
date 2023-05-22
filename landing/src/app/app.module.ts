b09a20c2caf8d3fb0360b4cff2683429b7238ae4
cpkopp12 <cameronpkopp@gmail.com>
2023-05-22 19:06:48 -0400

    weather module file structure

---
+++ landing/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { WeatherModule } from './weather/weather.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    WeatherModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
