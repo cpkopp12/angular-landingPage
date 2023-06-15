55c6835df979630b47a437d5ccdd1918f7f5c07c
cpkopp <cameronpkopp@gmail.com>
2023-06-14 15:13:55 -1000

    asnc bug in app template

--- landing/src/app/notifications/notification-list/notification-list.component.ts
+++ landing/src/app/notifications/notification-list/notification-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-notification-list',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./notification-list.component.css']
 })
 export class NotificationListComponent {
-
+  
 }
