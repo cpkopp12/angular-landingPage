47b8a67a6e8ca510b04da82713d70a3adbd07e7f
cpkopp <cameronpkopp@gmail.com>
2023-06-14 17:33:04 -1000

    structuring notification subject

--- landing/src/app/notifications/notifications.service.ts
+++ landing/src/app/notifications/notifications.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
-  constructor() { }
+  messages: Subject<any> | undefined;
+
+  constructor() {
+    this.messages = new Subject<any>();
+  }
+
+  addSuccess(message: string) {
+    this.messages?.next(message);
+  }
+
+  addError(message: string) {
+    this.messages?.next(message);
+  }
+
+  clearMessage(id: number) {
+
+  }
+
 }
